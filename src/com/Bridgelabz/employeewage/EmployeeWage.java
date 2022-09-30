74d8e4ec6f68ccc1b826be1c1676cf9fc5840817
srushtimane27 <srushtimane31@gmail.com>
2022-09-30 23:36:33 +0530

    Solve using switch case

--- src/com/Bridgelabz/employeewage/EmployeeWage.java
+++ src/com/Bridgelabz/employeewage/EmployeeWage.java
@@ -1,39 +1,48 @@
 package com.Bridgelabz.employeewage;
 
 /**
- * UC3: Add Part Time Employee And Wage ie. part time hour 8
+ * UC4: Solve using switch case statement
  */
 
 public class EmployeeWage {
 
-    static final  int WAGE_PER_HOUR = 20;
+    static final int WAGE_PER_HOUR = 20;
 
     static final int FULL_TIME_HOUR = 8;
 
     static final int PART_TIME_HOUR = 4;
 
+    static final int IS_FULL_TIME = 2;
+
+    static final int IS_PART_TIME = 1;
+
     public static void main(String[] args) {
 
-        int employeeAttendence = (int)Math.floor(Math.random() * 10) % 3;
+        int employeeAttendence = (int) Math.floor(Math.random() * 10) % 3;
         System.out.println(employeeAttendence);
 
         int dailyWage = 0;
 
-        if (employeeAttendence == 1){
+        switch (employeeAttendence){
 
-            System.out.println("Employee is Present");
-            dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+            case IS_FULL_TIME:
+                System.out.println("Employee is full time");
+                dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+                break;
 
-        } else if (employeeAttendence == 2) {
+            case IS_PART_TIME:
+                System.out.println("Employee is part time");
+                dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                break;
 
-            System.out.println("Employee is Part Time");
-            dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-
-        } else{
-
-            System.out.println("Employee is Absent");
+            default:
+                System.out.println("Absent");
         }
-        System.out.println("Daily Wage = "+dailyWage);
+
+        System.out.println("Daily Wage = " + dailyWage);
     }
+
+
+
 
 }
