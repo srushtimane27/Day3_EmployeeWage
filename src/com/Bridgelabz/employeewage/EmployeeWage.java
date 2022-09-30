5ec83184d184a40bd88be6588041aee7e763c979
srushtimane27 <srushtimane31@gmail.com>
2022-09-30 23:21:19 +0530

    Calculate daily employee wage

--- src/com/Bridgelabz/employeewage/EmployeeWage.java
+++ src/com/Bridgelabz/employeewage/EmployeeWage.java
@@ -1,19 +1,27 @@
 package com.Bridgelabz.employeewage;
 
 /**
- * UC1: Check Employee is Present or Absent
+ * UC2: Calculate Daily Employee Wage
  */
 
 public class EmployeeWage {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_TIME_HOUR = 8;
+
     public static void main(String[] args) {
 
         int employeeAttendence = (int)Math.round(Math.random());
         System.out.println(employeeAttendence);
 
+        int dailyWage = 0;
+
         if (employeeAttendence == 1){
             System.out.println("Employee is Present");
+            dailyWage = WAGE_PER_HOUR*FULL_TIME_HOUR;
         }else{
-            System.out.println("Employee is absent");
+            System.out.println("Employee is Absent");
         }
+        System.out.println("Daily Wage = "+dailyWage);
     }
+
 }
